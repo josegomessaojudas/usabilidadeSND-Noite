6163f8615287ed20914f3da1fe31089da149c1f4
josegomessaojudas <111508682+josegomessaojudas@users.noreply.github.com>
2022-09-22 19:24:40 -0300

    inicio_aula_22_09

---
+++ aula_09_22/terceiroprojeto/src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'terceiroprojeto';
+
+  textoBotao = "Esconder";
+  esconder = false;
+
+  pessoas = [
+    { nome: "José", idade: 18 },
+    { nome: "Maria", idade: 22 }
+  ];
+
+  adicionar(nome: any, idade: any) {
+    this.pessoas = [{ nome: nome, idade: idade }, ...this.pessoas];
+  }
+
+  alterarExibicao() {
+    this.textoBotao = this.esconder ? "Exibir" : "Esconder";
+    this.esconder = !this.esconder;
+  }
+}
