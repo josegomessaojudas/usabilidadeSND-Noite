ec8148f36cab74d214041b130a13761c854bdbc8
josegomessaojudas <111508682+josegomessaojudas@users.noreply.github.com>
2022-10-13 23:14:46 -0300

    Final _aula_13_10 Listagem 2.7.9
    iniciar em Listagem 2.8.1

---
+++ mean/src/app/clientes/cliente-inserir/cliente-inserir.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ClienteInserirComponent } from './cliente-inserir.component';
+
+describe('ClienteInserirComponent', () => {
+  let component: ClienteInserirComponent;
+  let fixture: ComponentFixture<ClienteInserirComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ClienteInserirComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ClienteInserirComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ mean/src/app/clientes/cliente-inserir/cliente-inserir.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cliente-inserir',
+  templateUrl: './cliente-inserir.component.html',
+  styleUrls: ['./cliente-inserir.component.css']
+})
+export class ClienteInserirComponent implements OnInit {
+
+  nome!: string;
+  fone!: string;
+  email!: string;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onAdicionarCliente() {
+    console.log('inserindo cliente...');
+    console.log(" variavel nome " + this.nome);
+  }
+
+}
